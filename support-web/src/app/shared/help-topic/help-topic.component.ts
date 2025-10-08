a138842c7e5a0b27d695930eaccc0fd4104abdf5
Johngardev <johngarlo@icloud.com>
2025-10-07 20:57:33 -0500

    se actualiza componente help-topic

--- support-web/src/app/shared/help-topic/help-topic.component.ts
+++ support-web/src/app/shared/help-topic/help-topic.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { HelpTopic } from '../../features/dashboard/components/summary/summary.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-help-topic',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './help-topic.component.html',
   styleUrl: './help-topic.component.css'
 })
